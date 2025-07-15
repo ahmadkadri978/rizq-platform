c210495e6bf8dae6d6373ef69627dc89d0020741
ahmadkadri978 <you@example.com>
2025-07-15 21:24:48 +0200

    Initial commit

---
+++ src/main/java/kadri/rizq_platform/RizqPlatformApplication.java
@@ -0,0 +1,13 @@
+package kadri.rizq_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RizqPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RizqPlatformApplication.class, args);
+	}
+
+}
